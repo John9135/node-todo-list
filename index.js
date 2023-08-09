c4ffe37a2206ffca35dcb9ed9cd9c2efb2fab764
John9135 <palacioalexander5@gmail.com>
2023-08-09 18:45:12 -0500

    feature: name of proyect en package and router in index

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const authorRouters = require('./routes/author')
-const bookRouters = require('./routes/book')
+const todoRouters = require('./routes/todo')
 
 const app = express()
 const port = 3005
 //middlewords
 app.set('port', port)
 app.use(express.json())
-app.use('/api/author', authorRouters)
-app.use('/api/book', bookRouters)
-
+app.use('/api/todo', todoRouters)
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.send('Todo list')
 })
 
-//app.post('/api/author/create',controllerAuthor.create)
+//app.post('/api/todo/create',controllerTodo.create)
 
 mongoose.connect(process.env.MONGO_DB_URI)
     .then(() => { console.log('conect to db') })
